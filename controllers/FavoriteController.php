f1e70486a0e9df6fe290153fa594dcc81de5cf02
Ignacio Menchaca <ignaciomenchaca666@gmail.com>
2019-02-21 20:00:09 -0600

    preparativos para favoritos

--- controllers/FavoriteController.php
+++ controllers/FavoriteController.php
@@ -2,8 +2,38 @@
 
 namespace FavoriteController;
 
+include '../config/Configuration.php';
+use Configuration\Configuration;
+use InterfaceModel\InterfaceController as Controller;
+use Utilities\Utilities;
+use Favorite\Favorite;
+Configuration::controller('Favorite');
 
-class FavoriteController
+class FavoriteController implements Controller
 {
 
+    public static function instance()
+    {
+        // TODO: Implement instance() method.
+    }
+
+    public static function save()
+    {
+        // TODO: Implement save() method.
+    }
+
+    public static function update()
+    {
+        // TODO: Implement update() method.
+    }
+
+    public static function destroy()
+    {
+        // TODO: Implement destroy() method.
+    }
+
+    public static function table()
+    {
+        // TODO: Implement table() method.
+    }
 }
---
+++ views/favorite/row.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: ignac
+ * Date: 21/02/2019
+ * Time: 07:58 PM
+ */
